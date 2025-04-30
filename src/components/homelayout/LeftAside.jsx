6bd7b81241686fc94c42bde573809f73d7d5fff2
ismail-dev-code <hm.ismail772@gmail.com>
2025-05-01 00:25:05 +0600

    chore: categories data load with use promise

--- src/components/homelayout/LeftAside.jsx
+++ src/components/homelayout/LeftAside.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { Suspense } from "react";
+import Categories from "../Categories";
 
 const LeftAside = () => {
-    return (
-        <div>
-            <h1>left aside</h1>
-        </div>
-    );
+  return (
+    <div>
+      <Suspense
+        fallback={<span className="loading loading-dots loading-xl"></span>}
+      >
+        <Categories></Categories>
+      </Suspense>
+    </div>
+  );
 };
 
 export default LeftAside;
--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -18,14 +18,14 @@ const HomeLayout = () => {
                     <Navbar></Navbar>
                 </nav>
             </header>
-            <main>
-              <aside>
+            <main className='w-11/12 mx-auto my-3 grid grid-cols-12'>
+              <aside className='col-span-3'>
                 <LeftAside></LeftAside>
               </aside>
-                <section className="main">
+                <section className="main col-span-6">
                     <Outlet></Outlet>
                 </section>
-               <aside>
+               <aside className='col-span-3'>
                 <RightAside></RightAside>
                </aside>
             </main>
