aaee6c05ac0317e31f13974f94c5ab29ef98262c
ismail-dev-code <hm.ismail772@gmail.com>
2025-05-01 09:09:17 +0600

    chore: filtered data for catergory

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,27 @@
-import React, { use } from 'react';
+import React, { use } from "react";
+import { NavLink } from "react-router";
 
-const categoryPromise = fetch("/categories.json").then(res=>res.json());
+const categoryPromise = fetch("/categories.json").then((res) => res.json());
 const Categories = () => {
-    const categories = use(categoryPromise);
-    return (
-        <div>
-            <h2 className='font-bold'>All Categories({categories.length})</h2>
-        </div>
-    );
+  const categories = use(categoryPromise);
+  return (
+    <div>
+      <h2 className="font-bold">All Categories({categories.length})</h2>
+      <div className="grid grid-cols-1 mt-5 gap-3">
+        {categories.map((category) => (
+          <NavLink
+            to={`/category/${category.id}`}
+            className={
+              "bg-base-100 btn border-0 font-semibold hover:bg-base-200 text-accent"
+            }
+            key={category.id}
+          >
+            {category.name}
+          </NavLink>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Categories;
